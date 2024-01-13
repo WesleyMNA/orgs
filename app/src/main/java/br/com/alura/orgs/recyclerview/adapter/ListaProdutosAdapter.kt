35da1551de68f54403b68a970e0fe0c7e8e3d519
Wesley Almeida <wesley.almeida@fotosensores.com>
2024-01-13 14:16:15 -0300

    wip

--- app/src/main/java/br/com/alura/orgs/recyclerview/adapter/ListaProdutosAdapter.kt
+++ app/src/main/java/br/com/alura/orgs/recyclerview/adapter/ListaProdutosAdapter.kt
@@ -4,13 +4,10 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import br.com.alura.orgs.R
+import br.com.alura.orgs.databinding.ProdutoItemBinding
 import br.com.alura.orgs.extensions.carregarImagem
 import br.com.alura.orgs.models.Produto
-import coil.load
 import java.text.NumberFormat
 import java.util.Locale
 
@@ -21,28 +18,28 @@ class ListaProdutosAdapter(
 
     private val dataSet = produtos.toMutableList()
 
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+    class ViewHolder(binding: ProdutoItemBinding) : RecyclerView.ViewHolder(binding.root) {
+        private val nome = binding.produtoItemNome
+        private val descricao = binding.produtoItemDescricao
+        private val valor = binding.produtoItemValor
+        private val imagem = binding.produtoItemImagem
+
         fun vincula(produto: Produto) {
-            val nome = itemView.findViewById<TextView>(R.id.produto_item_nome)
             nome.text = produto.nome
-            val descricao = itemView.findViewById<TextView>(R.id.produto_item_descricao)
             descricao.text = produto.descricao
-            val valor = itemView.findViewById<TextView>(R.id.produto_item_valor)
             val formatador = NumberFormat.getCurrencyInstance(Locale("pt", "br"))
             valor.text = formatador.format(produto.valor)
 
             if (produto.imagem == null)
                 itemView.visibility = View.GONE
 
-            val imagem = itemView.findViewById<ImageView>(R.id.produto_item_imagem)
-             imagem.carregarImagem(produto.imagem)
+            imagem.carregarImagem(produto.imagem)
         }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val inflater = LayoutInflater.from(context)
-        val view = inflater.inflate(R.layout.produto_item, parent, false)
-        return ViewHolder(view)
+        val binding = ProdutoItemBinding.inflate(LayoutInflater.from(context), parent, false)
+        return ViewHolder(binding)
     }
 
     override fun getItemCount(): Int = dataSet.size
